6f4821ea6756ec4a339ee3a4c603717630a857b4
kiran <kiran@Kiran>
2024-03-09 09:17:06 +0530

    First commit

---
+++ Practice/src/test/java/last/ToRunItTest.java
@@ -0,0 +1,13 @@
+package last;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToRunItTest {
+
+	@Test
+	public void toRun() {
+	
+		Reporter.log("Added",true);
+	}
+}
